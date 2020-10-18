d0de6f79a53a5849683d9d86a0a2841e2f8e6055
Kincent <kinlan19@bergen.org>
2020-10-17 23:48:32 -0400

    auth stuff doesnt work

---
+++ client/src/auth/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "../application/Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Test from "../Test";
+import Welcome from "../Welcome";
+import CreateClass from "../CreateClass"
+import RegisterClass from "../RegisterClass"
+import ChatDashboard from "../application/ChatDashboard";
+import Chat from "../application/Chat";
+import {AuthProvider} from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import Course from "../application/Course";
+
+const App = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="bodyDiv">
+                    <PrivateRoute exact path="/home" component={Home}/>
+                    <PrivateRoute exact path="/chat/:id" component={ChatDashboard}/>
+                    <PrivateRoute exact path="/chat/:id/:assignment/:question" component={Chat}/>
+                    <Route exact path="/" component={Welcome}/>
+                    <Route exact path="/test" component={Test}/>
+                    <Route exact path="/signup" component={SignUp}/>
+                    <Route exact path="/login" component={Login}/>
+                    <PrivateRoute exact path="/CreateClass" component={CreateClass}/>
+                    {/* <Route exact path="/createAssignment" component={CreateAssignment}/> */}
+                    <Route exact path="/registerclass" component={RegisterClass}/>
+                    <PrivateRoute exact path="/course/:id" component={Course}/>
+                </div>
+            </Router>
+        </AuthProvider>
+    );
+};
+
+export default App;
+    
---
+++ client/src/auth/Login.js
@@ -0,0 +1,62 @@
+import React, {useCallback, useContext, useState} from "react";
+import {withRouter, Redirect} from "react-router";
+import app from "../base.js";
+import "../css/navbar-login.css"
+import {AuthContext} from "./Auth.js";
+import {Link} from "react-router-dom";
+
+const Login = ({history}) => {
+    const [error, setError] = useState("");
+
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault();
+            const {email, password} = event.target.elements;
+            try {
+                await app
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                history.push("/home");
+            } catch (error) {
+                setError(error.toString());
+            }
+        },
+        [history]
+    );
+
+    const {currentUser} = useContext(AuthContext);
+
+    if (currentUser) {
+        return <Redirect to="/home"/>;
+    }
+
+    return (
+        <div id="loginPage">
+            <div className="navbar-login">
+                <button id="homeButton"><Link className="white" to="/">Home</Link></button>
+            </div>
+            <div class="formBox">
+                <h1>Log in</h1>
+                {error && (
+                    <div className="error">{error}</div>
+                )}
+                <div class="inputBox">
+                    <form onSubmit={handleLogin}>
+                        <div className="inputField">
+                            <label>Username</label>
+                            <input name="email" type="email" placeholder="Email"/>
+                        </div>
+                        <div className="inputField">
+                            <label>Password</label>
+                            <input name="password" type="password" placeholder="Password"/>
+                        </div>
+                        <button type="submit">Log in</button>
+                        <h5>Don't have an account? <Link to to="/signup">Sign up.</Link></h5>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default withRouter(Login);
